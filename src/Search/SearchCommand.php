485f119a7bd26c0271982fdb451ea4187351dcf9
david_smith <david_smith@sweetwater.com>
2025-01-29 20:10:32 -0500

    refactor/to-data-models Renamed `src/SearchCommand.php` to `src/Search/SearchCommand.php`.

--- src/Search/SearchCommand.php
+++ src/Search/SearchCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Zerotoprod\OmdbApiCli;
+namespace Zerotoprod\OmdbApiCli\Search;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -9,23 +9,24 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Zerotoprod\OmdbApi\OmdbApi;
-use Zerotoprod\OmdbApiCli\DataModels\SearchOptions;
 
 #[AsCommand(
-    name: 'omdb-api-cli:search',
+    name: SearchCommand::signature,
     description: 'Search for a movie, series or episode'
 )]
 class SearchCommand extends Command
 {
+    public const signature = 'omdb-api-cli:search';
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $Options = SearchOptions::from([...$input->getArguments(), ...$input->getOptions()]);
+        $Args = SearchArguments::from($input->getArguments());
+        $Options = SearchOptions::from($input->getOptions());
 
         $output->writeLn(
             json_encode(
-                (new OmdbApi($Options->apikey))->search(
-                    $Options->title,
+                (new OmdbApi($Args->apikey))->search(
+                    $Args->title,
                     $Options->type,
                     $Options->year,
                     $Options->page
@@ -39,8 +40,8 @@ class SearchCommand extends Command
 
     protected function configure(): void
     {
-        $this->addArgument(SearchOptions::apikey, InputArgument::REQUIRED, 'The API key.');
-        $this->addArgument(SearchOptions::title, InputArgument::REQUIRED, 'The ImdbID of the title.');
+        $this->addArgument(SearchArguments::apikey, InputArgument::REQUIRED, 'The API key.');
+        $this->addArgument(SearchArguments::title, InputArgument::REQUIRED, 'The ImdbID of the title.');
         $this->addOption(SearchOptions::type, mode: InputOption::VALUE_OPTIONAL, description: 'The type of the title: movie, series, episode', suggestedValues: ['movie', 'series', 'episode']);
         $this->addOption(SearchOptions::year, mode: InputOption::VALUE_OPTIONAL, description: 'The title year.');
         $this->addOption(SearchOptions::page, mode: InputOption::VALUE_OPTIONAL, description: 'The page number.');
