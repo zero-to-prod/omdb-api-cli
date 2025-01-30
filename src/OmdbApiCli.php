6542d12d9673b47101b7aea84e08c31a00c6d135
david_smith <david_smith@sweetwater.com>
2025-01-29 20:10:31 -0500

    refactor/to-data-models Updated `src/OmdbApiCli.php`.

--- src/OmdbApiCli.php
+++ src/OmdbApiCli.php
@@ -3,6 +3,10 @@
 namespace Zerotoprod\OmdbApiCli;
 
 use Symfony\Component\Console\Application;
+use Zerotoprod\OmdbApiCli\ByIdOrTitle\ByIdOrTitleCommand;
+use Zerotoprod\OmdbApiCli\Poster\PosterCommand;
+use Zerotoprod\OmdbApiCli\Search\SearchCommand;
+use Zerotoprod\OmdbApiCli\Src\SrcCommand;
 
 class OmdbApiCli
 {
